15e92b5854e2109b7ac93e2d556d016e1c732ec0
minhniee <nieework29@gmail.com>
2025-10-05 22:29:51 +0700

    chore(fonts) : change font to roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
+// import { GeistSans } from "geist/font/sans"
+// import { GeistMono } from "geist/font/mono"
+import { Roboto } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import "./globals.css"
+import { ro } from "date-fns/locale"
 
 export const metadata: Metadata = {
   title: "F-ALT - FPTU Admissions AI Labeling & Training Platform",
@@ -13,6 +15,15 @@ export const metadata: Metadata = {
   generator: "v0.app",
 }
 
+
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+  display: "swap",
+  style: ["normal", "italic"],
+})
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className={`${roboto.variable} font-roboto`}>
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
